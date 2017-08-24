d57a04e0254b63042c7b31944559a19b94c844bc
Nikita Novikov <s172926@wizard.uek.krakow.pl>
2017-08-24 18:32:25 +0400

    starting server on port 8081

--- server.js
+++ server.js
@@ -6,3 +6,21 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var firebase = require('firebase');
 var mysql = require('mysql');
 var url = require('url');
+
+/*
+
+												START SERVER
+
+*/
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Tobuy app listening at http://%s:%s", host, port)
+
+});
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
