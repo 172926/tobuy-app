fa285dcd3470dcc67c0858e2002e21ee4a30f2d5
Alex Omely <mykytanivikov@gmail.com>
2017-08-25 16:04:01 +0300

    requests added: list users, delete user, list lists, add user to group, add list

--- www/js.js
+++ www/js.js
@@ -1,21 +1,18 @@
 var ipaddr = 'http://192.168.0.102:8081';
 
-//Add logined user id to local storage
-
 if(localStorage.email === undefined || localStorage.email == ''){
-localStorage.email = prompt("Provide user email:");
+	localStorage.email = prompt("Provide user email:");
 
-$.ajax({
-	url: ipaddr + '/getId',
-	type: 'POST',
-	async: false,
-	data: {"email" : localStorage.email},
-	success: function(data){
-		localStorage.id = data[0];
-	},
-	error: function(e){//alert(e.message)
-	}
-});
+	$.ajax({
+		url: ipaddr + '/getId',
+		type: 'POST',
+		async: false,
+		data: {"email" : localStorage.email},
+		success: function(data){
+			localStorage.id = data[0];
+		},
+		error: function(e){}
+	});
 }
 
 $.ajax({
@@ -49,26 +46,115 @@ $.ajax({
 			$("#setGroup").append(content).collapsibleset('refresh');
 		
 			$('.btn-delete').click(function() {
-		//alert($(this).attr("data-group"));
-			$.ajax({
-				url: ipaddr + '/deleteGroup',
-				type: 'POST',
-				async: false,
-				data: {"groupName" : this.id, "id" : localStorage.id, "group_id" : $(this).attr("data-group")},
-				success: function(data){
-
-				},
-				error: function(e){alert(e.message)}
+				$.ajax({
+					url: ipaddr + '/deleteGroup',
+					type: 'POST',
+					async: false,
+					data: {"groupName" : this.id, "id" : localStorage.id, "group_id" : $(this).attr("data-group")},
+					success: function(data){},
+					error: function(e){alert(e.message)}
+				});
+			
+				location.reload();
 			});
-			location.reload();
-		});
-		
+			
+			var count = 0;
+			var datCount = 0;
+			var group_id = data[i + (data.length / 2)];
+			
+			$.ajax({
+				type: "POST",
+				url: ipaddr + '/listUsers',
+				data: ({group_id: data[i + (data.length / 2)]}),
+				async: true,
+				success: function(data){
+				
+					if(data != ""){	
+						var dat = data.length;
+						while(dat > 0){
+							document.getElementById('users'+count).innerHTML += data[dat-1].email + "";
+							var btn = document.createElement('a');
+							btn.setAttribute("class", "ui-btn ui-btn-inline ui-btn-c ui-corner-all ui-icon-delete ui-btn-icon-right ui-mini user_id");
+							btn.setAttribute("data-user_id", data[dat-1].id);
+							btn.setAttribute("data-group_id", group_id);
+							document.getElementById('users'+count).append(btn);
+							document.getElementById('users'+count).innerHTML += "<br>";
+							dat--;
+							datCount++;
+						}
+						count++;
+						datCount = 0;
+					}
+							
+					$('.user_id').click(function() {
+						$.ajax({
+							url: ipaddr + '/deleteUser',
+							type: 'POST',
+							async: false,
+							data: {"user_id" : $(this).attr('data-user_id'), "group_id" : $(this).attr('data-group_id')},
+							success: function(data){},
+							error: function(e){alert(e.message)}
+						});
+						location.reload();
+					});
+				}
+
+			});
+			
+			var count2 = 0;
+			var datCount2 = 0;
+			$.ajax({
+				type: "POST",
+				url: ipaddr+'/listListsGroups',
+				data: ({group_id: group_id}),
+				dataType: "json",
+				async: true,
+				success: function(data){
+					if(data != "undefined" && data != ""){
+						var dat = data.length;
+						while(dat > 0){
+							document.getElementById('lists'+count2).innerHTML += data[dat-1].list_name + "<br>";
+							dat--;
+							datCount++;
+						}
+						count2++;
+						datCount2 = 0;
+					}else{
+						document.getElementById('lists'+count2).innerHTML = "" + "<br>";
+					}
+				}
+			});
+			
+			$('.btn-add-user').click(function() {
+				var userEmail = prompt("Enter the user email: ");
+				$.ajax({
+					url: ipaddr + '/addUser',
+					type: 'POST',
+					async: false,
+					data: {"groupName" : this.id, "id" : localStorage.id, "userEmail" : userEmail},
+					success: function(data){},
+					error: function(e){alert(e.message)}
+				});
+				location.reload();
+			});
+			
+			$('.btn-add-list').click(function() {
+				var list_name = prompt("Enter the list name: ");
+				$.ajax({
+					url: ipaddr + '/addList',
+					type: 'POST',
+					async: false,
+					data: {"list_name" : list_name, "group_id" : $(this).attr('data-group')},
+					success: function(data){},
+					error: function(e){alert(e.message)}
+				});
+				location.reload();
+			});
 		}
 	},
 	error: function(e){alert(e.message)}
 });
 
-//Button create group
 $('#create-group').click(function() {
 
 	var groupName = prompt("Enter the group name: ");
@@ -89,7 +175,6 @@ $('#create-group').click(function() {
 	}
 });
 
-//Button logout
 $("#btn-logout").click(function() {
 
 	localStorage.clear();
