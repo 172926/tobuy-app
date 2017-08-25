9f9516f741f97ce33fa7780276577e300ec72b96
Nikita <mykytanivikov@gmail.com>
2017-08-25 15:02:26 +0300

    added: firebase js config, jquery-mobile, pgb.js

---
+++ www/js/pgb.js
@@ -0,0 +1,30 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+
+function deviceInfo() {
+
+	info =  'Hi, I am your smartphone :-)' + '\n' +
+			'=====' + '\n' +
+			'Device Name    : '     + device.name     + '\n' + 
+			'Device Cordova : '  + device.cordova + '\n' + 
+			'Device Platform: ' + device.platform + '\n' + 
+			'Device UUID    : '     + device.uuid     + '\n' + 
+			'Device Model   : '    + device.model     + '\n' + 
+			'Device Version : '  + device.version  + '\n';
+
+	navigator.notification.alert(info);
+	navigator.notification.beep(10);
+}
+
+function post() {
+
+	document.body.innerHTML += '<form id="dynForm" action="http://192.168.0.157:8081/process_post" method="post"><input type = "text" name = "first_name"></form>';
+	
+	document.getElementById("dynForm").submit();
+
+}
