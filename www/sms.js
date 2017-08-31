08abe841fb10ceedd64e5c74b0b174fd4aab7d7e
Nikita <mykytanivikov@gmail.com>
2017-08-31 19:41:09 +0300

    changed values in sms

--- www/sms.js
+++ www/sms.js
@@ -1,20 +1,21 @@
-
+var app{
     function sendSms(){
-	alert("Start SMS sending");
+	//alert("Start SMS sending");
         var number = document.getElementById('numberTxt').value;
         var message = document.getElementById('messageTxt').value;
-        //console.log("number=" + number + ", message= " + message);
+        console.log("number=" + number + ", message= " + message);
 
         //CONFIGURATION
         var options = {
-            //replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
             android: {
                 intent: 'INTENT'  // send SMS with the native android SMS messaging
                 //intent: '' // send SMS without open any other app
             }
         };
 
-        //var success = function () { alert('Message sent successfully'); };
-        //var error = function (e) { alert('Message Failed:' + e); };
+        var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
         sms.send(number, message, options);
     }
+}
