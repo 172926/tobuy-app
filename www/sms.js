f1e16354741c08b3621dea5df6eb9494912f65bf
Nikita <mykytanivikov@gmail.com>
2017-08-31 20:17:10 +0300

    added body navigation elements to sms.html

--- www/sms.js
+++ www/sms.js
@@ -13,8 +13,8 @@ var app = {
             }
         };
 
-        var success = function () { alert('Message sent successfully'); };
-        var error = function (e) { alert('Message Failed:' + e); };
+        var success = function () { /*alert('Message sent successfully'); */};
+        var error = function (e) { /*alert('Message Failed:' + e); */};
         sms.send(number, message, options, success, error);
     }
 };
