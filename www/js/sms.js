25a5e63c651b093f34deb7d17c1afeb83332cfe0
Nikita <mykytanivikov@gmail.com>
2017-08-31 19:07:30 +0300

    small fixes sms backup

--- www/js/sms.js
+++ www/js/sms.js
@@ -1,31 +1,4 @@
 
-//var ipaddr = 'http://54.226.242.116:8081';
-/*
-$.ajax({
-					url: ipaddr + '/sendSms',
-					type: 'POST',
-					async: false,
-					data: {"group_id" : localStorage.group_id},
-					success: function(data){
-						
-						var numberTxt = document.getElementById('numberTxt');
-						
-						
-						for(var i = 0; i < data.length; i++){
-						
-							numberTxt.value += data[i].phone_number + ",";
-						
-						}
-					
-					},
-					error: function(){
-					//alert(e.message)
-					}
-				});
-if(localStorage.group_id){
-localStorage.removeItem('group_id');
-}
-*/
 var app = {
     sendSms: function() {
         var number = document.getElementById('numberTxt').value;
