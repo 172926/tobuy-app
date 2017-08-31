20b7ec4c76acfbeddf783f36ffdcf698aa01ca47
Nikita <mykytanivikov@gmail.com>
2017-08-31 12:28:08 +0300

    fixed list items inside lists

--- www/js/lists.js
+++ www/js/lists.js
@@ -1,7 +1,7 @@
 
 var ipaddr = 'http://54.226.242.116:8081';
-
-
+//var ipaddr = 'http://192.168.0.102:8081'
+var list_count = 0;
 $.ajax({
 	url: ipaddr + '/getLists',
 	type: 'POST',
@@ -10,43 +10,44 @@ $.ajax({
 	success: function(data){
 		for (var i = 0; i < data.length / 3; i++) {
             var nextId = i;
-            nextId++;
+			nextId++;
             var content = "<div data-role='collapsible' data-theme='"+localStorage.theme+"' data-content-theme='"+localStorage.theme+"' id='list" + nextId + "'>" +
                 "<h2>" + data[i] + "</h2>" +
                 "<h3>Group: "+data[i + (data.length / 3)*2]+"</h3>" +
-				"<div id='form'></div>"+
+				"<div id='form"+list_count+"'></div>"+
                 "<div class='ui-grid-a'>" + "<div class='ui-block-a'>" +
-                "<a class='ui-btn ui-btn-inline ui-btn-b ui-corner-all ui-icon-gear ui-btn-icon-left ui-mini btn-edit-list' id=" + data[i] + " data-list_id=" + data[i + (data.length / 3)] + " href='#'>Edit list</a>" + "</div>" +
+                "<a class='ui-btn ui-btn-inline ui-btn-b ui-corner-all ui-icon-gear ui-btn-icon-left ui-mini btn-edit-list' id='" + data[i] + "' data-list_id=" + data[i + (data.length / 3)] + " href='#'>Edit list</a>" + "</div>" +
                 "<div class='ui-block-b'>" +
                 "<a class='ui-btn ui-btn-inline ui-btn-c ui-corner-all ui-icon-delete ui-btn-icon-right ui-mini btn-delete' id='" + data[i] + "' data-list_id=" + data[i + (data.length / 3)] + " href='#'>Delete list</a>" + "</div>" +
                 "</div>" + "</div>";
-			$("#setList").append(content).collapsibleset('refresh');
-			
-			$.ajax({
+				$("#setList").append(content).collapsibleset('refresh');
+				
+				$.ajax({
 				url: ipaddr + "/getItems",
 				type: 'POST',
-				async: true,
+				async: false,
 				data: {"list_id" : data[i + (data.length / 3)]},
 				success: function(data){
 					var counter = 0;
-
 					var data_counter = 0;
+					
 					for(var i = 0; i < data.length; i++){
 				
 					if(data[i].active == 0){
 						var content = "<div><input type='textfield' value='"+data[i].item_content+"' disabled />" + 
 						"</div>" +
 						"<input type='checkbox' checked disabled />"
-						$("#form").append(content);
+						$("#form"+list_count).append(content);
 					}else{
 						var content2 = "<div id='item-"+counter+"'><input type='textfield' id='data-"+data_counter+"' value='"+data[i].item_content+"' disabled />"+
 						"</div>" + 
 						"<input type='checkbox' id='item-active-"+counter+"' value="+data[i].id+" disabled  />"
-						$("#form").append(content2);
+						$("#form"+list_count).append(content2);
 						
 						counter++;
 						data_counter++;
 					}
+					
 					//var content = "<div id='item-"+counter+"'><input type='textfield' id='data-"+counter+"' value='"+data[i].item_content+"' disabled />" + 
 					//"</div>" + 
 					//"<input type='checkbox' id='"+data[i].item_content+"'><br>"
@@ -59,6 +60,10 @@ $.ajax({
 				},
 				error: function(e){}
 			})
+				list_count++
+			
+			
+			
 			
 			$('.btn-delete').click(function() {
 				$.ajax({
@@ -75,6 +80,7 @@ $.ajax({
 			$('.btn-edit-list').click(function() {
 				
 						localStorage.list_id = $(this).attr("data-list_id");
+						localStorage.editable = true;
 						window.location.href = "list-form.html";
 					
 				//location.reload();
