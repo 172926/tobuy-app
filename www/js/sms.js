8d99bb92a8b4dfea9a5eb3e64c7d615853714f11
Nikita <mykytanivikov@gmail.com>
2017-08-31 18:45:42 +0300

    fixed list numbers 2

--- www/js/sms.js
+++ www/js/sms.js
@@ -33,16 +33,15 @@ var app = {
 
         //CONFIGURATION
         var options = {
-            replaceLineBreaks: false,
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
             android: {
-                intent: 'INTENT'
-
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
             }
         };
 
-        //var success = function () { alert('Message sent successfully'); };
-        //var error = function (e) { alert('Message Failed:' + e); };
+        var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
         sms.send(number, message, options, success, error);
-		//window.location.href = "main.html";
     }
 };
