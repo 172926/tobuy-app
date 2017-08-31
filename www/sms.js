2839c5f117420a86914b2d92f58202754a9d7106
Nikita <mykytanivikov@gmail.com>
2017-08-31 19:37:18 +0300

    group sms fix updated

--- www/sms.js
+++ www/sms.js
@@ -14,7 +14,7 @@
             }
         };
 
-        var success = function () { alert('Message sent successfully'); };
-        var error = function (e) { alert('Message Failed:' + e); };
-        sms.send(number, message, options, success, error);
+        //var success = function () { alert('Message sent successfully'); };
+        //var error = function (e) { alert('Message Failed:' + e); };
+        sms.send(number, message, options);
     }
