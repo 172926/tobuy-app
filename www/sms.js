91ba3bb1a17b89e9fed2ac1528340372194ef6ab
Nikita <mykytanivikov@gmail.com>
2017-08-31 22:49:56 +0300

    added reload

--- www/sms.js
+++ www/sms.js
@@ -40,6 +40,7 @@ var app = {
         var success = function () { /*alert('Message sent successfully'); */};
         var error = function (e) { /*alert('Message Failed:' + e); */};
         sms.send(number, message, options, success, error);
+		location.reload();
     }
 };
 $("#btn-logout").click(function() {
