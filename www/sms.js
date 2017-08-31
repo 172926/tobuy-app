d0dd8be0d00ab8ac486b37d0bac744a1ede94480
Nikita <mykytanivikov@gmail.com>
2017-08-31 20:23:01 +0300

    added data-role & theme script to sms.html

--- www/sms.js
+++ www/sms.js
@@ -1,3 +1,27 @@
+
+$.ajax({
+					url: ipaddr + '/sendSms',
+					type: 'POST',
+					async: false,
+					data: {"group_id" : localStorage.group_id},
+					success: function(data){
+						
+						var numberTxt = document.getElementById('numberTxt');
+						
+						
+						for(var i = 0; i < data.length; i++){
+						
+							numberTxt.value += data[i].phone_number + ",";
+						
+						}
+					
+					},
+					error: function(){
+					//alert(e.message)
+					}
+});
+
+
 var app = {
     sendSms: function() {
         var number = document.getElementById('numberTxt').value;
